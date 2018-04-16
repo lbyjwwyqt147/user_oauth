deaba316f898fd2074dcca7ddecc475383478e34
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-04-16 23:00:00 +0800

    zTree

--- assets/snippets/pages/module/module_list.js
+++ assets/snippets/pages/module/module_list.js
@@ -4,9 +4,19 @@ var Module = function () {
     //== Private functions
     var ajaxUrl = "http://127.0.0.1:18081/oauth/v1/api/";
 
+    var zTreeObj;
+    // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
+    var setting = {};
+    // zTree 的数据属性，深入使用请参考 API 文档（zTreeNode 节点数据详解）
+    var zNodes = [
+        {name:"test1", open:true, children:[
+                {name:"test1_1"}, {name:"test1_2"}]},
+        {name:"test2", open:true, children:[
+                {name:"test2_1"}, {name:"test2_2"}]}
+    ];
     //资源菜单 grid
     var moduleTable = function () {
-
+        zTreeObj = $.fn.zTree.init($("#treeDemo"), setting, zNodes);
         var datatable = $('.m_datatable').mDatatable({
             // datasource definition
             data: {
